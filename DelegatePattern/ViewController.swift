3406b292454d6e93312fcbac8a979c7c3cc1a0b9
Marcin Karski <karski80@gmail.com>
2019-01-18 22:14:49 +0000

    Initial

--- DelegatePattern/ViewController.swift
+++ DelegatePattern/ViewController.swift
@@ -1,20 +1,14 @@
-//
-//  ViewController.swift
-//  DelegatePattern
-//
-//  Created by Marcin Karski on 18/01/2019.
-//  Copyright © 2019 Marcin Karski. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setup()
     }
-
-
+    
+    private func setup() {
+        view.backgroundColor = .white
+        self.title = "Delegate Pattern"
+    }
 }
-
