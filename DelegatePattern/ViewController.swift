cf51bf508f9c80d2efd7444b515423338d9500ce
Marcin Karski <karski80@gmail.com>
2019-01-19 12:33:50 +0000

    Further implementation

--- DelegatePattern/ViewController.swift
+++ DelegatePattern/ViewController.swift
@@ -1,5 +1,27 @@
 import UIKit
 
+protocol Controller {
+    associatedtype ViewType
+    var typedView: ViewType { get }
+}
+
+class BaseViewController<ViewType: UIView>: UIViewController, Controller {
+    
+    override func loadView() {
+        self.view = ViewType()
+    }
+    
+    var typedView: ViewType {
+        if let view = self.view as? ViewType {
+            return view
+        } else {
+            let view = ViewType()
+            self.view = view
+            return view
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
